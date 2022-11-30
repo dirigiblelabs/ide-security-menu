e526a9b02c528d7e9e45651de85ca28e7fbcf30e
StanZGenchev <stan.z.genchev@gmail.com>
2022-11-30 11:03:15 +0200

    Fixed templates

--- ide-security-menu/services/workspace-menu-new-template-access.js
+++ ide-security-menu/services/workspace-menu-new-template-access.js
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
 exports.getTemplate = function () {
@@ -20,6 +20,7 @@ exports.getTemplate = function () {
 					{
 						"path": "/myproject/myfolder/myservice.js",
 						"method": "GET",
+						"scope": "HTTP",
 						"roles": [
 							"administrator",
 							"operator"
@@ -28,7 +29,7 @@ exports.getTemplate = function () {
 				]
 			},
 			null,
-			2
+			4
 		)
 	};
 };
